470a10d9e2fed2ab2516e61b0011fbbbc44a42ae
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2023-06-08 16:29:38 -0300

    #9 Tornando o projeto instalavel cli (#12)
    closes #9

--- setup.py
+++ setup.py
@@ -6,4 +6,9 @@ setup(
     description="Reward Points System for Dunder Mifflin",
     author="Lucas Favaretto, Bruno Rocha(LinuxTips)",
     packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "dundie = dundie.__main__:main"
+        ]
+    }
 )
