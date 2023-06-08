99cde42380ae7c32db0d7de40f07711316168020
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2023-06-08 16:04:27 -0300

    #8 Configurando o ambiente de desenvolvimento (#11)
    closes #8

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dundie',
+    version='0.1.0',
+    description="Reward Points System for Dunder Mifflin",
+    author="Lucas Favaretto, Bruno Rocha(LinuxTips)",
+    packages=find_packages(),
+)
