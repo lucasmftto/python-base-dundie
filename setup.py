49038e4f6cb2dbc6c480f2c3b91780ad8f26ec84
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2023-06-18 16:08:15 -0300

    add GHA

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     author="Lucas Favaretto, Bruno Rocha(LinuxTips)",
+    python_requires=">=3.8",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
