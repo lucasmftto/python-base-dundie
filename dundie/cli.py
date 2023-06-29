1ae87654eb6596089ef8b309f131587bcfdfac0f
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2023-06-28 21:51:03 -0300

    add click, rich, rich-click para ficar com visual melhor, trabalhar melhor com comandos e facilidades

--- dundie/cli.py
+++ dundie/cli.py
@@ -1,30 +1,44 @@
-import argparse
+import pkg_resources
+import rich_click as click
+from dundie import core
+from rich import print
+from rich.console import Console
+from rich.table import Table
 
-from dundie.core import load  # noqa
+click.rich_click.USE_RICH_MARKUP = True
+click.rich_click.USE_MARKDOWN = True
+click.rich_click.SHOW_ARGUMENTS = True
+click.rich_click.GROUP_ARGUMENTS_OPTIONS = True
+click.rich_click.SHOW_METAVARS_COLUMN = False
+click.rich_click.APPEND_METAVARS_HELP = True
 
 
+@click.group()
+@click.version_option(pkg_resources.get_distribution("dundie").version)
 def main():
-    parser = argparse.ArgumentParser(
-        description="Dundie is a CLI tool.",
-        epilog="Enjoy and use with cautious.",
-    )
+    """Dundie System.
+    Dundie is a CLI tool.
+    """
 
-    parser.add_argument(
-        "subcommand",
-        type=str,
-        help="Subcommand to run.",
-        choices=("load", "show", "add", "send"),
-    )
 
-    parser.add_argument(
-        "filepath",
-        type=str,
-        help="File Path  to load.",
-    )
+@main.command()
+@click.argument("filepath", type=click.Path(exists=True))
+def load(filepath):
+    """Loads the file to the database.
 
-    args = parser.parse_args()
+    ## Features
 
-    try:
-        print(*globals()[args.subcommand](args.filepath))
-    except KeyError:
-        print("Invalid subcommand")
+    - Validates data
+    - Parses the file
+    - Loads the data to the database
+    """
+    table = Table(title="Dundie Mifflin Associates")
+    headers = ["name", "dept", "role", "email"]
+    for header in headers:
+        table.add_column(header, style="magenta")
+
+    result = core.load(filepath)
+    for person in result:
+        table.add_row(*[field.strip() for field in person.split(",")])
+    console = Console()
+    console.print(table)
--- integration/test_load.py
+++ integration/test_load.py
@@ -1,18 +1,17 @@
-from subprocess import CalledProcessError, check_output
-
 import pytest
+from click.testing import CliRunner
+from dundie.cli import load, main
+
+cmd = CliRunner()
 
 
 @pytest.mark.integration
 @pytest.mark.medium
 def test_load_positive_call_load_command():
     """integration test for load function"""
-    out = (
-        check_output(["dundie", "load", "./tests/assets/people.csv"])
-        .decode("utf-8")
-        .split("\n")
-    )
-    assert len(out) == 2
+    out = cmd.invoke(load, "./tests/assets/people.csv")
+
+    assert "Dundie Mifflin Associates" in out.output
 
 
 @pytest.mark.integration
@@ -20,8 +19,6 @@ def test_load_positive_call_load_command():
 @pytest.mark.parametrize("wrong_command", ["loady", "carrega", "start"])
 def test_load_negative_call_load_command(wrong_command):
     """integration test for load function"""
-    with pytest.raises(CalledProcessError) as error:
-        check_output(
-            ["dundie", wrong_command, "./tests/assets/people.csv"]
-        ).decode("utf-8").split("\n")
-    assert "status 2" in str(error.getrepr())
+    out = cmd.invoke(main, wrong_command, "./tests/assets/people.csv")
+    assert out.exit_code != 0
+    assert f"No such command '{wrong_command}'." in out.output
